4c3e09362fe973c1cc93ef10c9e9aefe0d4f6ae3
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-11-24 13:19:27 -0600

    Changing test runner for new gig

---
+++ bloom-grails-gradle-test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Get the upstream remote
+
+mega_repo="git@github.com:bloomhealth/bloomhealth.git"
+test_class="${1}"
+
+upstream=`git remote -v |grep 'upstream.*\(push\)' |cut -f2 |sed -e 's/ .*//'`
+if [ "${upstream}" == "${mega_repo}" ]; then
+    # Gradle build
+
+    # Test file
+    file=`find . -name ${test_class}.groovy | head -n 1 | sed -E 's#^\./##'`
+
+    if [ "${2}" == "debug" ]; then
+        debug="-Dtest.debug=true"
+    fi
+
+    # Phase (Bloom specific)
+    echo "Determining test phase for ${file}"
+    phase="test"
+    if ( echo "${file}" |grep -q -E '(plugins|webapps)/.*/test/unit/.*bloomhealth.*'); then
+        phase="grailsTestUnit"
+    elif ( echo "${file}" | grep -q -E '(plugins|webapps)/.*/test/integration/.*bloomhealth.*'); then
+        phase="grailsTestIntegration"
+    fi
+
+    # Project
+    if [[ ${file} == plugins* ]] || [[ ${file} == webapps* ]]; then
+        project=`echo ${file} |sed -e 's#/test/.*##'`
+    else
+        project=`echo ${file} |sed -e 's#/src/test/.*##'`
+    fi
+
+    echo "gradle -p ${project} -Dtest.single=${test_class} ${phase}"
+    gradle -p ${project} -Dtest.reportFormat=html,xml -Dtest.single=${test_class} ${phase} ${debug}
+
+    find ${project}/build -name "*${test_class}.xml"
+
+else
+    phase="integration"
+    file=`find test/unit -name ${test_class}.groovy`
+    if [ "$file" != "" ]; then
+        phase="unit"
+    fi
+
+    # Normal Grails stuff
+    echo "grails -Duser.timezone=UTC test-app ${phase}: ${test_class}"
+    grails -Duser.timezone=UTC test-app ${phase}: ${test_class}
+fi
--- grails-gradle-test.sh
+++ grails-gradle-test.sh
@@ -1,50 +1,19 @@
 #!/bin/bash
 
-# Get the upstream remote
-
-mega_repo="git@github.com:bloomhealth/bloomhealth.git"
 test_class="${1}"
+phase="test"
 
-upstream=`git remote -v |grep 'upstream.*\(push\)' |cut -f2 |sed -e 's/ .*//'`
-if [ "${upstream}" == "${mega_repo}" ]; then
-    # Gradle build
+# Test file
+file=`find . -name ${test_class}.groovy | head -n 1 | sed -E 's#^\./##'`
 
-    # Test file
-    file=`find . -name ${test_class}.groovy | head -n 1 | sed -E 's#^\./##'`
-
-    if [ "${2}" == "debug" ]; then
-        debug="-Dtest.debug=true"
-    fi
-
-    # Phase
-    echo "Determining test phase for ${file}"
-    phase="test"
-    if ( echo "${file}" |grep -q -E '(plugins|webapps)/.*/test/unit'); then
-        phase="grailsTestUnit"
-    elif ( echo "${file}" | grep -q -E '(plugins|webapps)/.*/test/integration'); then
-        phase="grailsTestIntegration"
-    fi
-
-    # Project
-    if [[ ${file} == plugins* ]] || [[ ${file} == webapps* ]]; then
-        project=`echo ${file} |sed -e 's#/test/.*##'`
-    else
-        project=`echo ${file} |sed -e 's#/src/test/.*##'`
-    fi
-
-    echo "gradle -p ${project} -Dtest.single=${test_class} ${phase}"
-    gradle -p ${project} -Dtest.reportFormat=html,xml -Dtest.single=${test_class} ${phase} ${debug}
-
-    find ${project}/build -name "*${test_class}.xml"
-
-else
-    phase="integration"
-    file=`find test/unit -name ${test_class}.groovy`
-    if [ "$file" != "" ]; then
-        phase="unit"
-    fi
-
-    # Normal Grails stuff
-    echo "grails -Duser.timezone=UTC test-app ${phase}: ${test_class}"
-    grails -Duser.timezone=UTC test-app ${phase}: ${test_class}
+if [ "${2}" == "debug" ]; then
+    debug="-Dtest.debug=true"
 fi
+
+# Project
+project=`echo ${file} |sed -e 's#/src/test/.*##'`
+
+echo "gradle -p ${project} -Dtest.single=${test_class} ${phase}"
+gradle -p ${project} -Dtest.reportFormat=html,xml -Dtest.single=${test_class} ${phase} ${debug}
+
+find ${project}/build -name "*${test_class}.xml"
